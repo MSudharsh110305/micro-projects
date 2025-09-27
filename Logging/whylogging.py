b3a8a681ecab2c972321be4e83082cf1aebde38f
MSudharsh110305 <sudharshm2005@gmail.com>
2025-09-27 18:44:02 +0530

    modified ignore file

---
+++ Logging/whylogging.py
@@ -0,0 +1,7 @@
+import logging
+
+# Print statement example:
+print("User logged in")  # Just a plain message to stdout
+
+# Logging example:
+logging.warning("User logged in")  # Logs with level WARNING, includes timestamp and other metadata
